fix MC simulation Greeks calculation

MCPricer.py
@@ -1,13 +1,10 @@
 import numpy as np
-np.random.seed(0)
-import math
-# from financial_instruments import
-# from scipy import stats
+# np.random.seed(0)
 
 class MCPricer:
     def __init__(self, option, drift, M):
         self.mu = drift
-        self.n = int(option.T * 365)  # number of steps (days)
+        self.n = option.T * 365  # number of steps (days)
         self.T = 1
         self.M = M  # num of sims
         self.S0 = option.S
@@ -15,52 +12,124 @@ class MCPricer:
         self.r = option.r
         self.option_class = option.option_class
         self.option = option
+        self.discount_factor = 1 / np.power((1 + self.r),self.n/365)
+        self.sum = 0
 
-    def GBM(self):
-        dt = self.T / self.n
+    def get_paths(self, S0, mu, sigma, n, M):
+        np.random.seed(0)
+        dt = 1 / n
         St = np.exp(
-            (self.mu - self.sigma ** 2 / 2) * dt + self.sigma * np.random.normal(0, np.sqrt(dt), size=(self.M, self.n)).T
+            (mu - sigma ** 2 / 2) * dt + sigma * np.random.normal(0, np.sqrt(dt), size=(int(M), int(n))).T
         )
-        St = np.vstack([np.ones(self.M), St])
-        St = self.S0 * St.cumprod(axis=0)
+        St = np.vstack([np.ones(M), St])
+        St = S0 * St.cumprod(axis=0)
         return St
 
-    def get_price(self):
-        simulation_paths = self.GBM()
+    def get_price_greeks(self):
+        self.sum = 0
+        simulation_paths = self.get_paths(self.S0, self.mu, self.sigma, self.n, self.M)
+        simulation_paths_delta = self.get_paths(self.S0+1, self.mu, self.sigma, self.n, self.M)
+        simulation_paths_gamma = self.get_paths(self.S0+2, self.mu, self.sigma, self.n, self.M)
+        simulation_paths_vega = self.get_paths(self.S0, self.mu, self.sigma+.01, self.n, self.M)
 
         if self.option_class == "european":
-            return np.average(self.option.get_payoff(simulation_paths[-1]) / (1 + self.r))
+            price = np.average(self.option.get_payoff(simulation_paths[-1]) * self.discount_factor )
+            price_delta = np.average(self.option.get_payoff(simulation_paths_delta[-1]) * self.discount_factor)
+            price_gamma = np.average(self.option.get_payoff(simulation_paths_gamma[-1]) * self.discount_factor )
+            price_vega = np.average(self.option.get_payoff(simulation_paths_vega[-1]) * self.discount_factor )
+
+            delta = price_delta - price
+            gamma = price_gamma - price - 2 * delta
+            vega = price_vega - price
+
+            return [price, delta, gamma, vega]
 
         elif self.option_class == "barrier":
             barrier = self.option.barrier
             barrier_type = self.option.barrier_type
-            sum = 0
+
             if barrier_type == "up-in":
-                for path in simulation_paths:
-                    if path.any() > barrier: #knock in
-                        sum += self.option.get_payoff(path[-1]) / (1 + self.r)
-                return sum/self.M
+                price = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths.T
+                                if np.sum(path > barrier) > 0]) / self.M
+                price_delta = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_delta.T
+                                if np.sum(path > barrier) > 0]) / self.M
+                price_gamma = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_gamma.T
+                     if np.sum(path > barrier) > 0]) / self.M
+                price_vega = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_vega.T
+                     if np.sum(path > barrier) > 0]) / self.M
+
+                delta = price_delta - price
+                gamma = price_gamma - price - 2*delta
+                vega = price_vega - price
+
+                return [price, delta, gamma, vega]
+
             elif barrier_type == "up-out":
-                for path in simulation_paths:
-                    out = False
-                    if path.any() > barrier: #knock out
-                        out = True
-                    if not out:
-                        sum += self.option.get_payoff(path[-1]) / (1 + self.r)
-                return sum/self.M
+                price = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths.T
+                                if np.sum(path > barrier) == 0]) / self.M
+                price_delta = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_delta.T
+                                if np.sum(path > barrier) == 0]) / self.M
+                price_gamma = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_gamma.T
+                                if np.sum(path > barrier) == 0]) / self.M
+                price_vega = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_vega.T
+                                if np.sum(path > barrier) == 0]) / self.M
+
+                delta = price_delta - price
+                gamma = price_gamma - price - 2*delta
+                vega = price_vega - price
+
+                return [price, delta, gamma, vega]
+
+
             elif barrier_type == "down-in":
-                for path in simulation_paths:
-                    if path.any() < barrier: # knock in
-                        sum += self.option.get_payoff(path[-1]) / (1 + self.r)
-                return sum/self.M
+                price = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths.T
+                                if np.sum(path < barrier) > 0]) / self.M
+                price_delta = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_delta.T
+                                if np.sum(path < barrier) > 0]) / self.M
+                price_gamma = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_gamma.T
+                                if np.sum(path < barrier) > 0]) / self.M
+                price_vega = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_vega.T
+                                if np.sum(path < barrier) > 0]) / self.M
+
+                delta = price_delta - price
+                gamma = price_gamma - price - 2*delta
+                vega = price_vega - price
+
+                return [price, delta, gamma, vega]
+
             elif barrier_type == "down-out":
-                for path in simulation_paths:
-                    out = False
-                    if path.any() < barrier: # knock out
-                        out = True
-                    if not out:
-                        sum += self.option.get_payoff(path[-1]) / (1 + self.r)
-                return sum/self.M
+                price = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths.T
+                                if np.sum(path < barrier) == 0]) / self.M
+                price_delta = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_delta.T
+                                if np.sum(path < barrier) == 0]) / self.M
+                price_gamma = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_gamma.T
+                                if np.sum(path < barrier) == 0]) / self.M
+                price_vega = np.sum(
+                    [self.option.get_payoff(path[-1]) * self.discount_factor for path in simulation_paths_vega.T
+                                if np.sum(path < barrier) == 0]) / self.M
+
+                delta = price_delta - price
+                gamma = price_gamma - price - 2*delta
+                vega = price_vega - price
+
+                return [price, delta, gamma, vega]
+
+
 
             elif self.option_class == "asian":
                 pass
@@ -73,11 +142,6 @@ class MCPricer:
 
             elif self.option_class == "rainbow":
                 pass
-
-
-
-
-
 
 
 

financial_instruments.py
@@ -6,11 +6,10 @@ class baseOption():
         '''
         :param underlying_price: S
         :param volatility: sigma
-        :param strike_price: X
+        :param strike_price: K
         :param time_to_expiration: T (days)
         :param risk_free_rate: r
         :param continuous_yield: q
-        :param barrier: b (barrier option)
         :param type:  call/put
         '''
         self.S = underlying_price
@@ -21,36 +20,22 @@ class baseOption():
         self.q = continuous_yield
         self.type = type
 
-    def get_d1_d2(self):
-        self.d1 = (np.log(self.S/self.K)+(self.r-self.q+np.power(self.sigma,2)/2)*self.T)/(self.sigma*np.sqrt(self.T))
-        self.d2 = self.d1 - self.sigma*np.sqrt(self.T)
+    def get_BS_price_greeks(self):
 
-    def BSPricer(self):
+        d1 = (np.log(self.S/self.K)+(self.r-self.q+np.power(self.sigma,2)/2)*self.T)/(self.sigma*np.sqrt(self.T))
+        d2 = d1 - self.sigma*np.sqrt(self.T)
+        gamma = np.exp(-self.q*self.T)*stats.norm.pdf(d1)/(self.S*self.sigma*np.sqrt(self.T))
+        vega = np.exp(-self.q*self.T)*self.S*np.sqrt(self.T)*stats.norm.pdf(d1)
 
         if self.type == "call":
-            self.price = self.S*np.exp(-self.q*self.T)*stats.norm.cdf(self.d1) - self.K*np.exp(-self.r*self.T)*stats.norm.cdf(self.d2)
-        elif self.type == "put":
-            self.price = self.K*np.exp(-self.r*self.T)*stats.norm.cdf(-self.d2) - self.S*np.exp(-self.q*self.T)*stats.norm.cdf(-self.d1)
+            price = self.S*np.exp(-self.q*self.T)*stats.norm.cdf(d1) - self.K*np.exp(-self.r*self.T)*stats.norm.cdf(d2)
+            delta = np.exp(-self.q*self.T)*stats.norm.cdf(d1)
         else:
-            raise ValueError("invalid option type, please try 'call' or 'put'. ")
-        return self.price
+            price = self.K*np.exp(-self.r*self.T)*stats.norm.cdf(-d2) - self.S*np.exp(-self.q*self.T)*stats.norm.cdf(-d1)
+            delta = -np.exp(-self.q*self.T)*stats.norm.cdf(-d1)
 
-    def get_delta(self):
-        if self.type == "call":
-            self.Delta = np.exp(-self.q*self.T)*stats.norm.cdf(self.d1)
-        elif self.type == "put":
-            self.Delta = -np.exp(-self.q*self.T)*stats.norm.cdf(-self.d1)
-        else:
-            raise ValueError("invalid option type, please try 'call' or 'put'. ")
-        return self.Delta
+        return [price, delta, gamma, vega]
 
-    def get_gamma(self):
-        self.Gamma = np.exp(-self.q*self.T)*stats.norm.pdf(self.d1)/(self.S*self.sigma*np.sqrt(self.T))
-        return self.Gamma
-
-    def get_vega(self):
-        self.Vega = np.exp(-self.q*self.T)*self.S*np.sqrt(self.T)*stats.norm.pdf(self.d1)
-        return self.Vega
 
     def get_payoff(self, predicted_price):
         if self.type == "call":
@@ -58,11 +43,13 @@ class baseOption():
         else:
             return np.clip(self.K - predicted_price, a_min=0, a_max=np.inf)
 
+
 class europeanOption(baseOption):
 
     def __init__(self, option_class = "european"):
         self.option_class = option_class
         super(baseOption, self).__init__()
+
 
 class barrierOption(baseOption):
 
@@ -76,21 +63,7 @@ class barrierOption(baseOption):
         self.barrier_type = barrier_type
         super(baseOption, self).__init__()
 
-    # def get_delta(self):
-    #     return 1
-    #
-    # def get_vega(self):
-    #     return 1
-    #
-    # def get_gamma(self):
-    #     return 1
 
 class rainbowOption(baseOption):
     def __init__(self):
         super(baseOption, self).__init__()
-    # def get_vega(self):
-    #     pass
-    # def get_gamma(self):
-    #     pass
-    # def get_delta(self):
-    #     pass